10924495a4e4a9ca0bfc84dbf7e0b9338c6aed7b
Son Hong <son.bjerg@gmail.com>
2020-06-12 09:59:52 +0700

    ODOO_RM_INTERN-016: Cau7: Show up code on cart: done

--- __manifest__.py
+++ __manifest__.py
@@ -27,6 +27,7 @@
         # 'security/ir.model.access.csv',
         'views/product_product_view.xml',
         'views/sale_order_view.xml',
+        'views/templates.xml',
         'wizard/update_product_warranty_view.xml',
     ],
     # only loaded in demonstration mode
